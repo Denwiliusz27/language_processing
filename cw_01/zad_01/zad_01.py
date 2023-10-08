b727daa4073b17a04383b72e44d0b171f9ba82d3
Denwiliusz27 <danwielgosz@gmail.com>
2023-10-08 12:54:44 +0200

    zad_01 - done

---
+++ cw_01/zad_01/zad_01.py
@@ -0,0 +1,17 @@
+import re
+
+if __name__ == '__main__':
+    # otwarcie pliku xml
+    f = open("370035.xml", "rt", encoding="utf8")
+
+    # regex
+    pattern = r'<provenance type="composed">\D*<placeName.*ref="(?P<y>.+)">(?P<x>.+)<\/placeName>'
+    # result = re.findall(regex, f.read())
+
+    # kompilowanie regexu do obiektu
+    regex = re.compile(pattern)
+
+    # wyszukuje tekst spełniający regex i zwraca znalezione fragmentyjako iteratory
+    for match in regex.finditer(f.read(), re.IGNORECASE | re.DOTALL):
+        print("Miejsce powstania dokumentu: ", match.group('x'))
+        print("Odnośnik do bazy danych: ", match.group('y'))
